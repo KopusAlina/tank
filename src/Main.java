13fad850c8b82aa01e72ae97cc3927bddff68b90
Alina Kopus <botton_of_the_ocean@mail.ru>
2024-02-17 16:25:25 +0300

    второй раз тест

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+
+    public static void main(String[] args) {
+        // At (0;0) fuel=100
+        final Tank justTank = new Tank();
+        // At (10;10) fuel=100
+        final Tank anywareTank = new Tank(10, 10);
+        // At (20;30) fuel=200
+        final Tank customTank = new Tank(20, 30, 200);
+        justTank.goForward(200);
+        justTank.printPosition();
+        anywareTank.goBackward(-200);
+        anywareTank.printPosition();
+        customTank.goForward(201);
+        customTank.printPosition();
+
+        Tank tank = new Tank();
+        tank.x = 1000;
+        tank.y = 2000;
+        tank.printPosition();
+    }
+}
+
